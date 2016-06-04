97c40840a30e2e922634a300a6e113fc61ad839f
sJhonny-e <sjhonny@inbox.com>
2016-06-04 17:36:44 +0100

    started to implement BuyItemAndGetReductionForAnotherItemDiscount

---
+++ ShoppingBasketDotNet.Tests/Models/Discounts/DiscountsTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using ShoppingBasketDotNet.Models;
+using ShoppingBasketDotNet.Models.Discounts;
+using ShoppingBasketDotNet.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketDotNet.Tests.Models.Discounts
+{
+    [TestFixture]
+    public class DiscountsTests
+    {
+        private static Item _joojiFruits = new Item(1, "jooji fruits", 1.0);
+        private static Item _popcorn = new Item(2, "popcorn", 2.5);
+
+
+        [Test]
+        public void GetDiscount_ForEmptyBasket_Returns_0()
+        {
+            var basket = new Mock<ShoppingBasket>(null);
+            var discount = new BuyItemAndGetReductionForAnotherItemDiscount(_joojiFruits, 3, _popcorn, 0.25);
+            Assert.AreEqual(0, discount.GetDiscount(basket.Object));
+        }
+    }
+}
--- ShoppingBasketDotNet/Models/Discounts/BuyItemAndGetReductionForAnotherItemDiscount.cs
+++ ShoppingBasketDotNet/Models/Discounts/BuyItemAndGetReductionForAnotherItemDiscount.cs
@@ -7,11 +7,24 @@ using System.Threading.Tasks;
 
 namespace ShoppingBasketDotNet.Models.Discounts
 {
-    class BuyItemAndGetReductionForAnotherItemDiscount : IDiscount
+    public class BuyItemAndGetReductionForAnotherItemDiscount : IDiscount
     {
+        private int _itemToBuyId;
+        private int _targetItemId;
+        private int _quantityToBuy;
+        private double _discountPercentage;
+
+        public BuyItemAndGetReductionForAnotherItemDiscount(Item itemToBuy, int quantity, Item targetItem, double discountPercentage)
+        {
+            _itemToBuyId = itemToBuy.Id;
+            _targetItemId = targetItem.Id;
+            _quantityToBuy = quantity;
+            _discountPercentage = discountPercentage;
+        }
+
         public double GetDiscount(ShoppingBasket shoppingBasket)
         {
-            throw new NotImplementedException();
+            return 0;
         }
     }
 }
