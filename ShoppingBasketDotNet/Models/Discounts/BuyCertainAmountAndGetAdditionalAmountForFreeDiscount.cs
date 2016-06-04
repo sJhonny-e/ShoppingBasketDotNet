2ed296749cac6322ce280e5949f9886f0e43b8bc
sJhonny-e <sjhonny@inbox.com>
2016-06-04 18:06:43 +0100

    another discount class with quite a long name

---
+++ ShoppingBasketDotNet/Models/Discounts/BuyCertainAmountAndGetAdditionalAmountForFreeDiscount.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingBasketDotNet.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketDotNet.Models.Discounts
+{
+    public class BuyCertainAmountAndGetAdditionalAmountForFreeDiscount : IDiscount
+    {
+        public double GetDiscount(ShoppingBasket shoppingBasket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
