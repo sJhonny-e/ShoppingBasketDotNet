920b47a343c3344111761faee8c2f2c96ab23893
sJhonny-e <sjhonny@inbox.com>
2016-06-04 17:01:51 +0100

    added discount class with the world's longest name

---
+++ ShoppingBasketDotNet/Models/Discounts/BuyItemAndGetReductionForAnotherItemDiscount.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingBasketDotNet.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketDotNet.Models.Discounts
+{
+    class BuyItemAndGetReductionForAnotherItemDiscount : IDiscount
+    {
+        public double GetDiscount(ShoppingBasket shoppingBasket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
