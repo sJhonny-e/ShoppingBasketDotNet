789fcd437429a7059c247709f8a0a74aa221632b
sJhonny-e <sjhonny@inbox.com>
2016-06-04 16:14:19 +0100

    added Item class and basic interface for Shopping basket

---
+++ ShoppingBasketDotNet/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketDotNet.Models
+{
+    public class Item
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Item(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ ShoppingBasketDotNet/Models/ShoppingBasket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketDotNet.Models
+{
+    public class ShoppingBasket
+    {
+        public void Add(Item item, int quantity)
+        {
+
+        }
+
+        public decimal CalculateTotal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
